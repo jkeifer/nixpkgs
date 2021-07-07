430c9aeece0a1c9b4c2bae017f8f96ae68c8dc69
jkeifer <jarrettkeifer@gmail.com>
2021-07-07 12:54:27 -0700

    refmt module, kitty workaround, fix homebrew path

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -3,46 +3,57 @@
 let
   mkIfCaskPresent = cask: lib.mkIf (lib.any (x: x == cask) config.homebrew.casks);
 in {
-  homebrew.enable = true;
-  homebrew.autoUpdate = true;
-  homebrew.cleanup = "zap";
-  homebrew.global.brewfile = true;
-  homebrew.global.noLock = true;
+  homebrew = {
+    enable = true;
+    autoUpdate = true;
+    cleanup = "zap";
 
-  homebrew.taps = [
-    "homebrew/cask"
-    "homebrew/cask-drivers"
-    "homebrew/cask-fonts"
-    "homebrew/cask-versions"
-    "homebrew/core"
-    "homebrew/services"
-  ];
+    # TODO: make this dependent on system value
+    brewPrefix = "/opt/homebrew/bin";
 
-  homebrew.masApps = {
-    Amphetamine = 937984704;
-    Keynote = 409183694;
-    Numbers = 409203825;
-    Pages = 409201541;
-    #Xcode = 497799835;
+    global = {
+      brewfile = true;
+      noLock = true;
+    };
+
+    taps = [
+      "homebrew/cask"
+      "homebrew/cask-drivers"
+      "homebrew/cask-fonts"
+      "homebrew/cask-versions"
+      "homebrew/core"
+      "homebrew/services"
+    ];
+
+    masApps = {
+      Amphetamine = 937984704;
+      Keynote = 409183694;
+      Numbers = 409203825;
+      Pages = 409201541;
+      #Xcode = 497799835;
+    };
+
+    casks = [
+      "1password"
+      "1password-cli"
+      "iterm2"
+      "firefox"
+      "google-chrome"
+      # `home-manager` currently has issues adding them to `~/Applications`
+      # Issue: https://github.com/nix-community/home-manager/issues/1341
+      "kitty"
+      "secretive"
+      "slack"
+      "wireshark"
+      "zoom"
+    ];
+
+    brews = [
+      # for cli commands not in nix
+    ];
   };
-
-  homebrew.casks = [
-    "1password"
-    "1password-cli"
-    "iterm2"
-    "firefox"
-    "google-chrome"
-    "secretive"
-    "slack"
-    "wireshark"
-    "zoom"
-  ];
 
   # Configuration related to casks
   environment.variables.SSH_AUTH_SOCK = mkIfCaskPresent "secretive"
      "/Users/${config.user.name}/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
-
-  homebrew.brews = [
-    # for cli commands not in nix
-  ];
 }
