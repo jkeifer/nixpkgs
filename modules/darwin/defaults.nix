9fb0b1b32b887bb74937a27e308774886b2a87e3
Jarrett Keifer <jkeifer0@gmail.com>
2024-01-26 15:00:03 -0800

    more macos defaults

--- modules/darwin/defaults.nix
+++ modules/darwin/defaults.nix
@@ -1,4 +1,5 @@
-{ ... }: {
+{ ... }: let
+in {
   system.defaults.NSGlobalDomain = {
     AppleInterfaceStyle = "Dark";
     AppleShowAllExtensions = true;
@@ -11,6 +12,7 @@
     NSNavPanelExpandedStateForSaveMode = true;
     NSNavPanelExpandedStateForSaveMode2 = true;
     "com.apple.sound.beep.feedback" = 0;
+    "com.apple.sound.beep.volume" = 0.725;
     "com.apple.trackpad.scaling" = 0.875;
     "com.apple.trackpad.enableSecondaryClick" = true;
   };
@@ -31,6 +33,13 @@
     static-only = true;
     show-recents = false;
     tilesize = 16;
+
+    # hot corners possible values
+    # https://daiderd.com/nix-darwin/manual/index.html#opt-system.defaults.dock.wvous-bl-corner
+    wvous-tl-corner = 1;
+    wvous-tr-corner = 1;
+    wvous-bl-corner = 1;
+    wvous-br-corner = 1;
   };
 
   system.defaults.loginwindow = {
@@ -50,4 +59,44 @@
     _FXShowPosixPathInTitle = true;
     FXEnableExtensionChangeWarning = true;
   };
+
+  system.defaults.CustomUserPreferences = {
+    "com.apple.TextEdit" = {
+      AddExtensionToNewPlainTextFiles = 0;
+      IgnoreHTML = 1;
+      RichText = 0;
+      SmartQuotes = false;
+    };
+
+    # still need to manually disable spotlight keyboard shortcut 😕
+    "com.raycast.macos" = {
+      raycastGlobalHotkey = "Command-49";
+    };
+
+    # maybe needs https://github.com/LnL7/nix-darwin/pull/825 but even that doesn't work
+    #"com.if.Amphetamine" = {
+    #  "Hide Dock Icon" = 1;
+    #  "Icon Style" = 2;
+    #  "Trigger Data" = [
+    #    {
+    #      Enabled = 1;
+    #      Name = "Wifi";
+    #      SSID = "Othrostichies";
+    #      "Screen Saver Delay" = 5;
+    #      "Screen Saver Exceptions" = [];
+    #      TypeIDs = [
+    #        2
+    #      ];
+    #    }
+    #  ];
+    #};
+  };
+
+  system.activationScripts.postUserActivation.text = ''
+    # diable startup sound
+    sudo /usr/sbin/nvram StartupMute=%01
+
+    # following line should allow us to avoid a logout/login cycle for most settings
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
 }
